978de9335805c25e982d42af61dc855b52b59d19
v8phan <56612604+v8phan@users.noreply.github.com>
2022-03-31 15:03:14 -0500

    db.define not a function debugging; hot reloading

--- monkeyserver/config/database.js
+++ monkeyserver/config/database.js
@@ -1,4 +1,6 @@
+
 const { Sequelize } = require('sequelize');
+
 
 // module.exports = new Sequelize('main', 'root', 'password', {
 //   host: 'localhost',
--- monkeyserver/models/User.js
+++ monkeyserver/models/User.js
@@ -1,24 +1,46 @@
-const Sequelize = require("sequelize");
-const db = require('../config/database');
+// const Sequelize = require("sequelize");
+// const db = require('../config/database');
 
-const User = db.define("user", {
+const sequelize = require('database.js').sequelize;
+const DataTypes = require('mysql');
+
+const User = sequelize.define('user'), {
   email: {
-    type: Sequelize.STRING,
+    type: DataTypes.STRING,
     allowNull: false,
   },
   password: {
-    type: Sequelize.STRING,
+    type: DataTypes.STRING,
     allowNull: false,
   },
   id: {
-    type: Sequelize.INTEGER,
+    type: DataTypes.INTEGER,
     autoIncrement: true,
     primaryKey: true,
   },
   testcolumn: {
-    type: Sequelize.INTEGER,
+    type: DataTypes.INTEGER,
   },
-});
+}
+
+// const User = db.define("user", {
+//   email: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   },
+//   password: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   },
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     primaryKey: true,
+//   },
+//   testcolumn: {
+//     type: Sequelize.INTEGER,
+//   },
+// });
 
 User.associate = (models) => {
   User.hasMany(models.Comments, {
