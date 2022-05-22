a95ae97ea200f4d5dc8765fe770fe0d403c031ea
v8phan <56612604+v8phan@users.noreply.github.com>
2022-05-21 19:16:23 -0500

    moved monkeyclient, app.js header (cors)

--- monkeyserver/app.js
+++ monkeyserver/app.js
@@ -6,6 +6,15 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 const db = require("./models/index.js");
 const User = require("./models/User");
 const Comment = require("./models/Comment");
@@ -41,7 +50,7 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-app.use('/api', require('./routes/login'));
-app.use('/api', require('./routes/signup'));
-app.use('/api', require('./routes/postcomment'));
-app.use('/api', require('./routes/displaycomment'));
+app.use("/api", require("./routes/login"));
+app.use("/api", require("./routes/signup"));
+app.use("/api", require("./routes/postcomment"));
+app.use("/api", require("./routes/displaycomment"));
