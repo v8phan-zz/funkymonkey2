105537f534a9569aa33f8314db92ffa31dc8ce95
v8phan <56612604+v8phan@users.noreply.github.com>
2022-05-23 18:37:56 -0500

    added cors to dependencies (i think)

--- monkeyserver/app.js
+++ monkeyserver/app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const cors = require('cors')
 
-app.use(cors())
+app.use(cors());
 // app.use(function (req, res, next) {
 //   res.header('Access-Control-Allow-Origin', '*');
 //   res.header(
